f96dbc5b00fe0f25ad3eeafbe8f198260dafaf6f
Thomas Konings <tkon99@gmail.com>
2014-04-30 21:14:21 +0200

    new working update (base64 data images)

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <?php
 include('phpqrcode/qrlib.php'); 
-$imagedata = file_get_contents('small.jpg');
-$original = strlen($imagedata);
-$imagedata = urlencode($imagedata);
+$image = 'small.jpg';
+$fd = fopen($image, 'rb');
+$size= filesize($image);
+$imagedata = fread($fd, $size);
+$origpicdata = $imagedata;
+fclose ($fd);
+$original = $size;
+$imagedata = base64_encode($imagedata);
 $encoded = strlen($imagedata);
 $tempDir = "tmp/";
-QRcode::png($imagedata, $tempDir.'qr.png', QR_ECLEVEL_L); 
+$data = 'data:image/jpeg;base64,';
+$data .= $imagedata;
+QRcode::png($data, $tempDir.'qr.png', QR_ECLEVEL_L);
 ?>
 <h1>Image QR-Code</h1><br>
+<h2>How to: scan qr code --> copy contents & paste as url in browser</h2>
 <b>Image data:</b><br>
+Original:<br>
+<?php echo $origpicdata; ?><br><br>
+Encoded (Base64):<br>
 <?php
 echo $imagedata;
 ?>
@@ -22,5 +33,8 @@ Length safe transfer encoded:
 echo $encoded;
 ?>
 <hr>
-<b>Output QR-code</b><br>
+<b>(Base64 data)Image output</b><br>
+<img src="data:image/jpeg;base64,<?php echo($imagedata); ?>">
+<hr>
+<b>Data QR code</b><br>
 <img src="<?php echo($tempDir); ?>qr.png">
